51a4e265c77083ab72ea4f0a81025c34cb18dad4
aydinhuseynov <aydinnhuseynli@gmail.com>
2023-11-14 00:29:20 +0400

    artificial intelligence navbar section by react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+
+<Navbar/>
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+import '../assets/style/Button.css'
+
+const Button = () => {
+  return (
+    <button className='navbar-button m-2 btn btn-primary'>
+      My Button
+    </button>
+  )
+}
+
+export default Button
